5eed6070aa8afec278c0438969fb78decb581526
Ashley Meah <ashley@meah.me.uk>
2016-08-20 23:15:50 +0100

    public build 3
    Small quick update, fixed up routing generally. 404 not found, default
    homepage etc
    Initial repository base for storing/initilizing model variables

--- application/init.php
+++ application/init.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * PHPRouter
+ *
+ * @copyright Ashley Meah (http://ashley.meah.me.uk/)
+ * @license   The MIT License (MIT)
+ *
+ * This source file is subject to the The MIT License (MIT) License that is bundled
+ * with this source code in the file LICENSE.md
+ */
 
-require 'application/baseApp.class.php';
+//autoload application classes
+spl_autoload_register(
+	function ($name)
+	{
+		if(file_exists('application/' . $name . '.class.php'))
+			require_once('application/' . $name . '.class.php');
+	}
+);
+
+//instantiate MVC
 $application = new baseApp;
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php 
+/**
+ * PHPRouter
+ *
+ * @copyright Ashley Meah (http://ashley.meah.me.uk/)
+ * @license   The MIT License (MIT)
+ *
+ * This source file is subject to the The MIT License (MIT) License that is bundled
+ * with this source code in the file LICENSE.md
+ */
 
 require_once 'application/init.php';
 
